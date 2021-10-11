cebe97c86bf4f802986cdf88b9d4eaa541abe15c
Ido Strauss <strauss02@hotmail.co.il>
2021-10-12 01:30:49 +0300

    init webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
-// const path = require('path')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
 
-// module.exports = {
-//     entry: "",
-//     output: {
-//         filename: "app.js",
-//         path:
-//     }
-// }
+module.exports = {
+  entry: './solution/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './solution/index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+}
